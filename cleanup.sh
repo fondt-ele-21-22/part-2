0809b33436435096a92b57d5b51e4193261d56ce
Michele Lombardi <michele.lombardi2@unibo.it>
2022-03-01 11:58:42 +0100

    Added unsolved exercise

--- cleanup.sh
+++ cleanup.sh
@@ -3,13 +3,13 @@
 if [ "$(git branch --show-current)" = "main" ]; then
     if [ -z "$(git status --porcelain=v1 2>/dev/null)" ]; then
         echo "SEEK AND DESTROY!!!"
-        # rm *-sol.ipynb
-        # rm pdfs/*-sol.pdf
-        # rm -rf *-dev
-        # rm docker-compose.yml
-        # rm Dockerfile
-        # rm print-pdf.sh
-        # rm cleanup.sh
+        rm *-sol.ipynb
+        rm pdfs/*-sol.pdf
+        rm -rf *-dev
+        rm docker-compose.yml
+        rm Dockerfile
+        rm print-pdf.sh
+        rm cleanup.sh
     fi
 fi
 
